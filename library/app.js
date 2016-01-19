1b9eecaf2a1d6c57490c895130c0f82be06ee852
c.neculita <c.neculita@levi9.com>
2016-01-19 14:57:31 +0200

    Controllers

--- library/app.js
+++ library/app.js
@@ -1,27 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 
-var sql = require('mssql');
-var config={
-    user:'sa',
-    password:'Smart000',
-    server: 'localhost\\SQLEXPRESS',
-    database:'Books'
-};
+// var sql = require('mssql');
+// var config={
+//     user:'sa',
+//     password:'Smart000',
+//     server: 'localhost\\SQLEXPRESS',
+//     database:'Books'
+// };
 
-sql.connect(config, function(err){
-   if(err)
-    {console.log(err);}
-    
-});
+// sql.connect(config, function(err){
+//    if(err)
+//     {console.log(err);}
+//     
+// });
 
 var port = process.env.port ||5000;
 var nav = [{Link:'/Books',Text: 'Book'}, {Link:'/Authors', Text:'Author'}];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret:'library'}));
+
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 // var handlebars = require('express-handlebars');
@@ -31,13 +44,16 @@ app.set('view engine', 'ejs');
 
 
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
+
 
 app.get('/', function(req, res){
    res.render('index', {title: 'Hello from render',nav: nav}); 
 });
 
 
-
 app.listen(port, function(err){
     console.log('running server on port: '+port);
 });
+
